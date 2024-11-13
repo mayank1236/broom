2e01d85e9cdaa30a08cc7c0c00d19814a59a1c8d
Mayank Gupta <mayankgupta14042000@gmail.com>
2024-11-14 00:15:09 +0530

    new packages and some files moved

---
+++ frontend/src/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import {
+  types,
+  version,
+  Device,
+  detectDevice,
+  parseScalabilityMode,
+  debug
+} from "mediasoup-client";
+
+
+function App() {
+
+  return (
+    <p>
+      main
+    </p>
+  )
+}
+
+export default App
