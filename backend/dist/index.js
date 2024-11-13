dd1bdfa8b92e270ef335970a745360ec622c088e
Mayank Gupta <mayankgupta14042000@gmail.com>
2024-11-13 18:19:29 +0530

    docker files init

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,17 +1,14 @@
-import { WebSocketServer } from "ws";
-
-const wss = new WebSocketServer({ port: 8080 });
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const ws_1 = require("ws");
+const wss = new ws_1.WebSocketServer({ port: 8080 });
 // let senderSocket: null | WebSocket = null;
 // let receiverSocket: null | WebSocket = null;
-
 wss.on('connection', function connection(ws) {
     // ws.on('error', console.error);
-
-    ws.on('message', function message(data: any) {
+    ws.on('message', function message(data) {
         const message = JSON.parse(data);
-        console.log(message)
+        console.log(message);
     });
-
     // ws.send('something');
 });
