ace310ef31f981e601a6c4e28f1b4beb694c00d6
Mayank Gupta <mayankgupta14042000@gmail.com>
2024-11-14 10:33:50 +0530

    setting up frontend structure with react-router-dom

---
+++ frontend/src/routes/router.tsx
@@ -0,0 +1,21 @@
+import Root, { loader as rootLoader } from './root.tsx';
+import ErrorPage from "./error-page.tsx";
+import { createBrowserRouter } from 'react-router-dom';
+import Contact from './contact.tsx';
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Root />,
+      errorElement: <ErrorPage />,
+      loader: rootLoader,
+      children: [
+        {
+          path: "contacts/:contactId",
+          element: <Contact />,
+        },
+      ],
+    },
+]);
+
+  export default router;
